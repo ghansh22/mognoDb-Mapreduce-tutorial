4b462cbddae82900d531fd29fc3ad3b3384b76ec
Ghanshyam Baviskar <gnbaviskar@gmail.com>
2018-02-11 19:22:40 +0530

    mapR basics

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,96 @@
 const routes = require('express').Router();
+const mongojs = require('mongojs');
+const db = mongojs('mapreduce', ['classes']);
 
+routes.get('/', (req, res) => {
+    // res.send('test');
+
+    /***************************************************************************
+     * map basics
+     */
+    var students = [
+        { name: "Dale Cooper", class: "Calculus", tests: [30, 28, 45] },
+        { name: "Harry Truman", class: "Geometry", tests: [28, 26, 44] },
+        { name: "Shelly Johnson", class: "Calculus", tests: [27, 26, 43] },
+        { name: "Bobby Briggs", class: "College Algebra, tests: [20, 18, 35]" },
+        { name: "Donna Heyward", class: "Geometry", tests: [28, 28, 44] },
+        { name: "Audrey Horne", class: "College Algebra", tests: [22, 26, 44] },
+        { name: "James Hurley", class: "Calculus", tests: [20, 20, 38] },
+        { name: "Lucy Moran", class: "College Algebra", tests: [26, 24, 40] },
+        { name: "Tommy Hill", class: "College Algebra", tests: [30, 29, 46] },
+        { name: "Andy Brennan", class: "Geometry", tests: [20, 21, 38] }
+    ];
+
+    let studInfo = students.map((x) => {
+        return x.name + 'is in ' + x.class;
+    });
+    // res.json(studInfo);
+
+
+    /************************************************************************************
+     reduce basics
+   ******/
+    let tests = [
+        { score: 30 },
+        { score: 28 },
+        { score: 45 }
+    ];
+
+    // accenpts key, value, 
+    // 1st param is for aggreagation
+    // 2nd param is for iteration
+    // sum is set to 0 initially
+    let testSum = tests.reduce((sum, tests) => {
+        return sum + tests.score;
+    }, 0);
+    // res.json(testSum);
+});
+
+
+
+routes.get('/mapR1', (req, res) => {
+    /*************************************************************************************
+     * sample map job 
+     */
+    var mapFunc = function () {
+        for (i = 0; i < this.students.length; i++) {
+            var student = this.students[i];
+
+            // Emit gets 2 arguments being the key on which you want to group the 
+            // data and the data itself
+            emit(student.fName + " " + student.lName, 1);
+        }
+    };
+
+    /*************************************************************************************
+     * sample reduce job 
+     */
+    var reduceFunc = function (student, values) {
+        count = 0;
+
+        for (i = 0; i < values.length; i++) {
+            count += values[i];
+        }
+        return count;
+    };
+
+    db.classes.mapReduce(mapFunc, reduceFunc, { out: "map_ex" });
+    db.map_ex.find((err, result) => {
+        if(err){res.json(err)}else{
+            if(result){
+                res.json(result);
+            }else{res.json('no result!')}
+        }
+    });
+
+
+
+    /*************************************************************************************
+     * excluding reducer 
+     */
+    // var reduceFunc = function (student, values) {
+    //     return values[0]
+    // };
+});
 
 module.exports = routes;
